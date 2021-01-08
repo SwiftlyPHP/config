acf9687480685ef42d34d326b2db9f1bd24a8c88
clvarley <conorlukevarley@gmail.com>
2021-01-08 14:19:08 +0000

    Added get, set and has methods to config store

--- Store.php
+++ Store.php
@@ -4,6 +4,9 @@ namespace Swiftly\Config;
 
 use Swiftly\Config\LoaderInterface;
 
+use function strtolower;
+use function array_key_exists;
+
 /**
  * Container used to store and manipulate config values
  *
@@ -42,5 +45,49 @@ Class Store
         $loader->load( $this );
     }
 
-    // TODO: get, set, has
+
+    /**
+     * Gets the value for the given setting
+     *
+     * If no value is found, the provided $default will be returned instead.
+     *
+     * @param string $key    Setting name
+     * @param mixed $default (Optional) Default value
+     * @return mixed         Setting value
+     */
+    public function get( string $key, $default = null ) // : mixed
+    {
+        $key = strtolower( $key );
+
+        return ( array_key_exists( $key, $this->values )
+            ? $this->values[$key]
+            : $default
+        );
+    }
+
+    /**
+     * Sets the value for the given setting
+     *
+     * @param string $key  Setting name
+     * @param mixed $value Setting value
+     * @return void        N/a
+     */
+    public function set( string $key, $value ) : void
+    {
+        $key = strtolower( $key );
+        $this->values[$key] = $value;
+
+        return;
+    }
+
+    /**
+     * Checks to see if the given setting has a value
+     *
+     * @param string $key Setting name
+     * @return bool       Has value?
+     */
+    public function has( string $key ) : bool
+    {
+        return array_key_exists( $key, $this->values );
+    }
 }
