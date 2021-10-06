71d57696e661d15bed8b2f0492d830ba8fe9e090
clvarley <conorlukevarley@gmail.com>
2021-10-07 00:32:45 +0100

    Stub out JsonLoader tests

---
+++ tests/Loader/JsonLoaderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Swiftly\Config\Tests\Loader;
+
+use Swiftly\Config\Loader\JsonLoader;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group Unit
+ */
+Class JsonLoaderTest Extends TestCase
+{
+    
+}
