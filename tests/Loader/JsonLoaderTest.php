9f933552ebb59e2eceaa267ed87498dc597fced8
clvarley <conorlukevarley@gmail.com>
2021-10-07 01:25:24 +0100

    Started writing tests for the JsonLoader

--- tests/Loader/JsonLoaderTest.php
+++ tests/Loader/JsonLoaderTest.php
@@ -3,12 +3,60 @@
 namespace Swiftly\Config\Tests\Loader;
 
 use Swiftly\Config\Loader\JsonLoader;
+use Swiftly\Config\Store;
 use PHPUnit\Framework\TestCase;
+
+use function file_put_contents;
+use function json_encode;
 
 /**
  * @group Unit
  */
 Class JsonLoaderTest Extends TestCase
 {
-    
+
+    const TEMP_FILE = 'php://memory';
+
+    const EXAMPLE_JSON = [
+        'id' => 123,
+        'name' => 'John',
+        'nested' => [
+            'life' => 42,
+            'example' => 'some_value'
+        ]
+    ];
+
+    /** @var JsonLoader $loader */
+    private $loader;
+
+    public static function setUpBeforeClass() : void
+    {
+        file_put_contents( self::TEMP_FILE, json_encode( self::EXAMPLE_JSON ));
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        file_put_contents( self::TEMP_FILE, '' );
+    }
+
+    protected function setUp() : void
+    {
+        $this->loader = new JsonLoader( self::TEMP_FILE );
+    }
+
+    public function testLoadsValuesFromJsonFile() : void
+    {
+        $store = $this->createMock( Store::class );
+
+        $store->expects( $this->once() )
+            ->method( 'set' )
+            ->with(
+                $this->equalTo( 'id' ),
+                $this->equalTo( 123 )
+            );
+
+        // TODO:
+
+        $this->loader->load( $store );
+    }
 }
